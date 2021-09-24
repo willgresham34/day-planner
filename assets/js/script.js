ea94c6495a3982aea65b5cb2d3697cbc8db79826
Will Gresham <willgresham34@gmail.com>
2021-09-24 16:18:12 -0400

    cant get function to work but pushing to save

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,29 @@
  var today = moment();
  $('#currentDay').text(today.format("dddd, MMMM Do"));
 
+var container = document.querySelector(".container");
+
+
+// for loop that sorts through all time blocks on load
+var  timeBlocks = $("#9, #10, #11, #12, #13, #14, #15, #16, #17")
+// var  timeBlocks = $("#9", "#10", "#11", "#12", "#13", "#14", "#15", "#16", "#17")
+var currentHour = moment().format("H");
+
+ console.log(currentHour);
+ console.log(timeBlocks);
+
+
+for (i = 0; i < timeBlocks.length; i++) {
+    if ( [i] == currentHour) {
+        $(timeBlocks[i]).addClass("present");
+    } else if ( i < currentHour) {
+        $(timeBlocks[i]).addClass("past");
+    } else {
+        $(timeBlocks[i]).addClass("future");
+    }
+}
+  container.addEventListener("click", function(event) {
+
+  })
+
+ 
