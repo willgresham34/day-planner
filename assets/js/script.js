6fdc38dc1443b7831d779574896fef3dc0bbd353
Will Gresham <willgresham34@gmail.com>
2021-09-24 22:20:27 -0400

    Finished product

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,21 +3,32 @@ var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
 
 var container = document.querySelector(".container");
-
-
 var currentHour = moment().format("H");
+var saveConfirm = document.querySelector("#saveConfirm");
 
 //for loop for changing colors of time-blocks
 for (var i = 9; i < 18; i++) {
   if (i < currentHour) {
-    document.getElementById(eval(i)).classList.add("past");
-  } else if (i === currentHour) {
-    document.getElementById(eval(i)).classList.remove("past");
-    document.getElementById(eval(i)).classList.add("present");
+    document.getElementById(i).classList.add("past");
+  } else if (i == currentHour) {
+    document.getElementById(i).classList.remove("past");
+    document.getElementById(i).classList.add("present");
   } else {
-    document.getElementById(eval(i)).classList.remove("present");
-    document.getElementById(eval(i)).classList.add("future");
+    document.getElementById(i).classList.remove("present");
+    document.getElementById(i).classList.add("future");
   }
+  document.getElementById(i).value = localStorage.getItem(i)
 }
 
-container.addEventListener("click", function (event) {});
+container.addEventListener("click", function (event) {
+    var targetString = JSON.stringify(event.target.id)
+    if(targetString.includes('but')){
+
+    var blankSave = JSON.parse(targetString.replace('but', ''))
+    var inputTodo = document.getElementById(blankSave)
+    
+    localStorage.setItem(blankSave, inputTodo.value)
+
+    }
+    document.getElementById("saveConfirm").innerHTML = "Your item has been saved!";
+});
